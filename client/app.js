bc882af6fe85c2592488a1226a91f61cbcf583b6
Marta Nara <marta.kos1230@gmail.com>
2022-04-05 19:23:08 +0200

    Add ChatBot

--- client/app.js
+++ client/app.js
@@ -1,4 +1,11 @@
 
+const socket = io();
+
+// listener
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('join', (name) => addMessage('ChatBot', `${name} has joined the conversation!`));
+socket.on('removeUser', (name) => addMessage('ChatBot', `${name} has left the conversation...`));
+
 let userName = ""
 
 const loginForm = document.querySelector('#welcome-form');
@@ -15,6 +22,7 @@ const login = e => {
     userName = userNameInput.value;
     messagesSection.classList.add('show');
     loginForm.classList.remove('show');
+    socket.emit('join', { name: userName })
   } else {
     alert('Please enter username')
   };
@@ -26,8 +34,12 @@ loginForm.addEventListener('submit', e => {
 
 const sendMessage = e => {
   e.preventDefault();
-  if(messageContentInput.value){
-    addMessage(userName, messageContentInput.value);
+
+  let messageContent = messageContentInput.value;
+
+  if(messageContent){
+    addMessage(userName, messageContent);
+    socket.emit('message', { author: userName, content: messageContent })
     messageContentInput.value = '';
   } else {
     alert('Message field can\'t be empty!')
@@ -38,6 +50,7 @@ const addMessage = (author, content) => {
   const message = document.createElement('li');
   message.classList.add('show', 'message--received');
   if (author === userName) message.classList.add('message--self');
+  if (author === 'ChatBot') message.classList.add('message--chatbot');
   message.innerHTML = `
     <h3 class="message__author">${author === userName ? 'You' : author}</h3>
     <div class="message__content">
@@ -50,3 +63,4 @@ const addMessage = (author, content) => {
 addMessageForm.addEventListener('submit', e => {
   sendMessage(e);
 })
+
--- server.js
+++ server.js
@@ -1,7 +1,39 @@
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
 const app = express();
+
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const socket = require('socket.io');
+const io = socket(server);
+
+const messages = [];
+const users = [];
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('message', (message) => { 
+    console.log('Oh, I\'ve got something from ' + socket.id)
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+});
+  socket.on('join', ({ name }) => {
+    users.push({ name, id: socket.id });
+    socket.broadcast.emit('join', name);
+  });
+  socket.on('disconnect', () => {
+    console.log('Socket ' + socket.id + ' has left');
+    const found = users.find(user => user.id === socket.id)
+    if(found){
+      socket.broadcast.emit('removeUser', found.name);
+      users.splice(users.indexOf(found), 1)
+    }
+  });
+});
+
+// const cors = require('cors');
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -18,8 +50,4 @@ app.get('*', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('Page not found...');
 });
-
-
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
-});
+  
